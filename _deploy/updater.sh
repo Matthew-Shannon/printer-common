642b24c001eab27fef4ef7bf095d8c892df90a37
Matthew Shannon <mshannon93@gmail.com>
2026-07-29 15:33:10 -0500

    updates

--- _deploy/updater.sh
+++ _deploy/updater.sh
@@ -33,7 +33,7 @@ fi
 # --- Remote Execution ---
 # The -tt option forces a pseudo-terminal, which is useful for scripting.
 # The 'heredoc' (<< EOF) sends the enclosed block of commands to the remote machine.
-sshpass -p "${SSH_PASSWORD}" ssh -o StrictHostKeyChecking=accept-new -tt "${PRINTER_USER}@${PRINTER_HOST}" 'bash -s' << EOF
+sshpass -p "${SSH_PASSWORD}" ssh -o StrictHostKeyChecking=accept-new "${PRINTER_USER}@${PRINTER_HOST}" 'bash -s' << EOF
     # Exit immediately if a command exits with a non-zero status.
     set -e
 
@@ -41,7 +41,7 @@ sshpass -p "${SSH_PASSWORD}" ssh -o StrictHostKeyChecking=accept-new -tt "${PRIN
     cd "${KLIPPER_CONFIG_REPO_PATH}"
 
     echo "--> Pulling newest changes from Git repository..."
-    git pull
+    git pull -q
 
     echo "--> Restarting Klipper via Moonraker API..."
     # These send POST requests to the local Moonraker instance on the printer.
