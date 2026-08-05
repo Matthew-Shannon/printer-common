a6183485372258801d7da98b55d829e500b072f9
Matthew Shannon <mshannon93@gmail.com>
2026-08-05 14:31:47 -0500

    asdf

--- _deploy/push.sh
+++ _deploy/push.sh
@@ -4,6 +4,6 @@ git add .
 git commit -m "updates"
 git push
 
-#bash ./_deploy/updater.sh artemisprinter.lan biqu
+bash ./_deploy/updater.sh artemisprinter.lan biqu
 bash ./_deploy/updater.sh charlieprinter.lan biqu
 bash ./_deploy/updater.sh dennisprinter.lan biqu
